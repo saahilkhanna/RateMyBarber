d1e1fa5a7f92ab13db5399d4d037629adfbb5366
Saahil Khanna <saahilkhanna100@gmail.com>
2024-08-10 15:53:50 -0400

    Rate my barber project

--- backend/models/Barber.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BarberSchema = new mongoose.Schema({
-    name: String,
-    barbershop: { type: mongoose.Schema.Types.ObjectId, ref: 'Barbershop' },
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
-});
-
-module.exports = mongoose.model('Barber', BarberSchema);
